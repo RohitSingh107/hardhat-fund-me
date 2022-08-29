96b794d2241ccfb4a6ea79bcaf3c67906fafcde4
Rohit <RohitSinghEmail@protonmail.com>
2022-08-29 23:28:05 +0530

    some changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,12 @@
 import * as dotenv from "dotenv"
-
 import { HardhatUserConfig, task } from "hardhat/config"
 import "@nomiclabs/hardhat-etherscan"
 import "@nomiclabs/hardhat-waffle"
-import "@typechain/hardhat"
 import "hardhat-gas-reporter"
 import "solidity-coverage"
 import "hardhat-deploy"
 import "@nomiclabs/hardhat-ethers"
 import "dotenv/config"
-
-dotenv.config()
 
 const config: HardhatUserConfig = {
     solidity: {
